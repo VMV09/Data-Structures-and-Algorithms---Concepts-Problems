1e1d1ab66ec2bf5ee04504a70b6d937d35f2a9ec
Vishruth M V <113712071+VMV09@users.noreply.github.com>
2025-06-23 18:39:36 +0530

    Add files via upload

---
+++ Arrays/LargestElement.py
@@ -0,0 +1,18 @@
+# Given an array arr of n integers,
+# the task is to find the largest element in the given array.
+
+def findlargest(a):
+    largest = a[0]
+    for i in range(len(a)):
+        if a[i] > largest:
+            largest = a[i]
+    return largest
+
+a = [10, 33, 21, 78, 12]
+res = findlargest(a)
+print(res)
+
+# Follows an Iterative Approach
+# Time Complexity -> O(n)
+# This time cannot be reduced for finding largest element in an unsorted array.
+# O(n) remains the best time complexity that can be achieved.
