3fc43f5e8315ed9e2949ee523aa8a7a31cad33dc
Vishruth M V <113712071+VMV09@users.noreply.github.com>
2025-06-28 14:41:41 +0530

    Printing Alternate Elements in an Array

---
+++ Arrays/Problem 04.py
@@ -0,0 +1,10 @@
+def getAlternatives(a):
+    for i in range(len(a)):
+        if i%2==0:
+            print(a[i], end=" ")
+
+# A Sample Test Case
+a = [10, 20, 30, 40, 50, 60]
+getAlternatives(a)
+
+# You can define your own Test Cases and Check the working and correctness of the code.
