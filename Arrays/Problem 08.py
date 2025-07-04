40c4a18e77ef565381131e185926dddfc8877f12
Vishruth M V <113712071+VMV09@users.noreply.github.com>
2025-07-04 11:09:58 +0530

    Rotate Array by k steps

--- Arrays/Problem 08.py
+++ Arrays/Problem 08.py
@@ -1,5 +1,14 @@
+# Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+# Input: nums = [1,2,3,4,5,6,7], k = 3
+# Output: [5,6,7,1,2,3,4]
+# Explanation:
+#     rotate 1 steps to the right: [7,1,2,3,4,5,6]
+#     rotate 2 steps to the right: [6,7,1,2,3,4,5]
+#     rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
 nums = [1, 2, 3, 4, 5, 6, 7]
 k = 3
+# Design your own test cases to check and verify the working of the code
 
 for i in range(k):
     n = nums.pop()           # pop last element
