e6a95ba2e89ee03f3176a9cb3b3dbd2b6bb76bb4
Vishruth M V <113712071+VMV09@users.noreply.github.com>
2025-07-05 08:40:50 +0530

    Second Largest Element

--- Arrays/Problem 09.py
+++ Arrays/Problem 09.py
@@ -21,3 +21,21 @@ def secondlargest(arr):
 arr = [10, 3, 5, 9, 12, 16, 17]
 res = secondlargest(arr)
 print(res)
+
+# The same question can be solved using a one pass search with two pointers.
+# Time Complexity => O(n); as we will only traverse the array once and keep a track of largest and secondlargest.
+def secondlargest(arr):
+  n = len(arr)
+  largest = -1
+  secondlargest = -1
+  for i in range(n):
+    if arr[i] > largest:
+      secondlargest = largest
+      largest = arr[i]
+    elif arr[i] < largest and arr[i] > secondLargest:
+      secondLargest = arr[i]
+  return secondlargest
+
+arr = [10, 3, 5, 9, 12, 16, 17]
+res = secondlargest(arr)
+print(res)
